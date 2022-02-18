94a1a3232e552bae254a60d4a0e10509b31f3aaf
Betsi Rojas <betsi060414@gmail.com>
2022-02-17 22:13:51 -0500

    aplicar boots page - contact

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ let menu_icon = document.querySelector("#menu-icon");
  menu_icon.addEventListener("click",function(){
      menu.classList.toggle("navbar__menu--toggle")
  })
+
+ // Fetch all the forms we want to apply custom Bootstrap validation styles to
+ var forms = document.querySelectorAll('.needs-validation')
+
+ // Loop over them and prevent submission
+ Array.prototype.slice.call(forms)
+   .forEach(function (form) {
+     form.addEventListener('submit', function (event) {
+       if (!form.checkValidity()) {
+         event.preventDefault()
+         event.stopPropagation()
+       }
+
+       form.classList.add('was-validated')
+     }, false)
+   })
